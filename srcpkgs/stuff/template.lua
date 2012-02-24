9e3bd43d38a2a84e4bbd11994d1377e786cf75a9
davehome <davehome@redthumb.info.tm>
2012-02-24 12:25:58 -0700

    Update test template

--- srcpkgs/stuff/template.lua
+++ srcpkgs/stuff/template.lua
@@ -5,19 +5,16 @@ return {
 	version = "0.0.1",
 	revision = 2,
 	homepage = "http://stellarbox.co.cc",
+	distfiles = { "$homepage/$name-$version.tar.xz" },
+	patches = { "fix_stuff.patch" },
+	checksums = { "d99089a63a6cfc1a6a4a026be9278028d47d224088d24b1853acb67e95683a15",
+		"89a63a6cfc1a6a4a026be9278028d47d2249278028d47d224088d24b1853acbe" },
 	license = "GPL-3",
 	maintainer = "davehome <davehome@redthumb.info.tm>",
 	summary = "This is the stuff program",
 	description = { "The stuff program lorem ipsum blah blah blah and stuff liek",
 			"that or somethign like that. And all that kind of stuff",
-			"like that."
-	},
-	distfiles = { "$homepage/$name-$version.tar.xz" },
-	patches = { "fix_stuff.patch" },
-	checksums = {
-		"d99089a63a6cfc1a6a4a026be9278028d47d224088d24b1853acb67e95683a15",
-		"89a63a6cfc1a6a4a026be9278028d47d2249278028d47d224088d24b1853acbe"
-	},
+			"like that." },
 	deps = {
 		build = { "sed", "coreutils", "libfoo-devel", "libbar-devel" },
 		run = { "libfoo.so.3", "libbar.so.1", "ld-linux.so.2", "libc.so.6" },
