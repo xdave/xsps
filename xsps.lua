3eb3e5d05b43b0156224638374af126f05da93e8
davehome <davehome@redthumb.info.tm>
2012-02-23 21:00:57 -0700

    xsps.lua: test getopts

--- xsps.lua
+++ xsps.lua
@@ -4,6 +4,68 @@ require "posix"
 
 local argv = arg
 
+local show_help = function()
+	local n = posix.basename(arg[0])
+	local msg = [[
+usage: %s [-h] [-c FILE] [<action> PACKAGE]
+ -h		display this help message.
+ -c FILE	load alternate configuration from FILE.
+
+ <action>	is one of: `fetch', `verify', `extract',
+		`configure', `build' or `install`.
+		NOTE: only `test' is implemented right now.
+
+PACKAGE		is the package template you would like to
+		perform <action> on.]]
+	print(string.format(msg, n))
+	os.exit(1)
+end
+
+local load_config = function(f)
+	print("TODO: load config: "..f)
+end
+
+local tasks = {}
+
+local options = {
+	["-h"] = show_help,
+	["-c"] = load_config,
+	["test"] = "process_template"
+}
+
+if not arg[1] then show_help() end
+
+for i, opt in ipairs(arg) do
+	local option = options[opt]
+	if option then
+		if option == load_config then
+			if arg[i+1] then
+				option(arg[i+1])
+				table.remove(arg, i)
+				table.remove(arg, i+1)
+			else
+				show_help()
+			end
+		end
+		if option == show_help then
+			show_help()
+		end
+		if option == "process_template" then
+			if arg[i+1] then
+				table.insert(tasks, { action = "test", tmpl = arg[i+1] })
+				table.remove(arg, i)
+				table.remove(arg, i+1)
+			end
+		end
+	else
+		show_help()
+	end
+end
+
+table.foreach(tasks, function(k,v)
+	print(k, v.action, v.tmpl)
+end)
+
 xsps = {
 	format = {
 		env = function(str)
@@ -14,6 +76,7 @@ xsps = {
 		end
 	},
 	CONFIGDIR = function()
+		-- currently based on dir xsps in run in
 		local working = posix.dirname(arg[0])
 		return xsps.format.env(working.."/config")
 	end,
@@ -47,8 +110,6 @@ xsps.inc = function(file)
 	return func()
 end
 
-xsps.process_queue = {}
-
 xsps.config = xsps.inc(xsps.CONFIGDIR().."/xsps.lua")
 xsps.color = xsps.inc(xsps.getcfg("TOOLSDIR").."/misc/color.lua")
 xsps.log = xsps.inc(xsps.getcfg("TOOLSDIR").."/misc/log.lua")
@@ -62,4 +123,7 @@ xsps.ni = function(msg)
 	local err = "[%s] Not implemented yet!"
 	xsps.log.err(string.format(err, msg))
 end
+
+xsps.process_queue = {}
+
 
