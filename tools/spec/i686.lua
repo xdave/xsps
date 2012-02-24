d11553f5935cf5abeceb5489edd9f0580f244907
davehome <davehome@redthumb.info.tm>
2012-02-24 15:26:36 -0700

    update specs

--- tools/spec/i686.lua
+++ tools/spec/i686.lua
@@ -3,6 +3,5 @@ return {
 	base = "void",
 	arch = "i686",
 	triplet = "$arch-$vendor-$kernel-$os",
-	multilib = false
-
+	supported = { "i686" }
 }
--- tools/spec/x86_64.lua
+++ tools/spec/x86_64.lua
@@ -3,6 +3,5 @@ return {
 	base = "void",
 	arch = "x86_64",
 	triplet = "$arch-$vendor-$kernel-$os",
-	multilib = true
-
+	supported = { "x86_64", "i686" }
 }
