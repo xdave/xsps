a76982c0c20c532566629b091d8a6be74fd15558
davehome <davehome@redthumb.info.tm>
2012-02-25 14:59:34 -0700

    Args: cleanup; use PROGNAME

--- classes/Args.lua
+++ classes/Args.lua
@@ -1,18 +1,21 @@
 local lapp = require "tools/lapp/lapp"
 module(...,package.seeall)
 
-function lapp.callback(parm,arg,args)
+function lapp.callback(parm, arg, args)
 	log:debug("+"..parm..": "..arg)
 end
 
-Args = class(function(a)
-	a.items = lapp [[Usage: xsps.lua [options].. ACTION [TEMPLATE]
+Args = class(function(self)
+	local str = [[Usage: xsps.lua [options].. ACTION [TEMPLATE]
 Options:
-  -c,--config	(default ./config/xsps.lua) ==> Config file to use
-  <action>	(string) ==> One of 'install', 'remove', 'clean'
-  <template>	(default bootstrap) ==> Template
+  -c,--config	(default config/xsps.lua)	Config file to use
+  <action>	(string)			One of 'install', 'remove',
+							or 'clean'
+  <template>	(default bootstrap)		Template
 
   Use -h or --help to display this message.
 ]]
+	local help_text = string.format(str, PROGNAME)
+	self.items = lapp(help_text)
 end)
 
