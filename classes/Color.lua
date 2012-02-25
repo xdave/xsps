9f3960be7ea1134a238360da5cf837f5b56888f4
davehome <davehome@redthumb.info.tm>
2012-02-25 15:00:04 -0700

    Color: change 'c' to 'self' in contructor

--- classes/Color.lua
+++ classes/Color.lua
@@ -1,13 +1,13 @@
 module(..., package.seeall)
 
 -- global
-Color = class(function(c)
-	c.off = 0 c.bold = 1 c.italic = 3 c.underline = 4
-	c.inverse = 7 c.hidden = 8 c.black = 30 c.red = 31
-	c.green = 32 c.yellow = 33 c.blue = 34 c.magenta = 35
-	c.cyan = 36 c.white = 37 c.black_bg = 40 c.red_bg = 41
-	c.green_bg = 42 c.yellow_bg = 43 c.blue_bg = 44
-	c.magenta_bg = 45 c.cyan_bg = 46 c.white_bg = 47
+Color = class(function(self)
+	self.off = 0 self.bold = 1 self.italic = 3 self.underline = 4
+	self.inverse = 7 self.hidden = 8 self.black = 30 self.red = 31
+	self.green = 32 self.yellow = 33 self.blue = 34 self.magenta = 35
+	self.cyan = 36 self.white = 37 self.black_bg = 40 self.red_bg = 41
+	self.green_bg = 42 self.yellow_bg = 43 self.blue_bg = 44
+	self.magenta_bg = 45 self.cyan_bg = 46 self.white_bg = 47
 end)
 
 function Color:get(color)
