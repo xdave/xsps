c7d22b44d363195bbdc3a5afd2c7a1eb92f5c74d
davehome <davehome@redthumb.info.tm>
2012-02-24 19:35:41 -0700

    tools: get rid of some stuff.. re-writing

--- tools/vinstall.lua
+++
@@ -1,6 +0,0 @@
--- This function is not implemented at the moment.
-function vinstall(from, mode, to)
-	xsps.ni("vinstall")
-end
-
-return vinstall
--- tools/vmove.lua
+++
@@ -1,4 +0,0 @@
--- This function is not implemented at the moment.
-return function()
-	xsps.ni("vmove")
-end
