0472647fb42e711ab601a0fc9468b3c4c7f4460e
davehome <davehome@redthumb.info.tm>
2012-02-26 16:58:26 -0700

    main: test Args()

--- src/xsps.cc
+++ src/xsps.cc
@@ -1,8 +1,15 @@
 #include <common.h>
 #include <Log.h>
 #include <parse_args.h>
+#include <classes/Args.h>
 
 int main(int argc, char **argv) {
+	// This is a test
+	xsps::Args args(argc, argv);
+	for(auto item : args.args) {
+		fprintf(stdout, "%s\n", item);
+	}
+
 	xsps::LogTypes LOG;
 	xsps::Log(LOG.WARN, "This program is currently useless.", "FIXME");
 	return xsps::parse_args(LOG, argc, argv);
