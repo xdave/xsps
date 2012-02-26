e716b29ccfc070f018a342407d4fda7d574e4ef6
davehome <davehome@redthumb.info.tm>
2012-02-26 15:20:25 -0700

    xsps.cxx: don't use 'using namespace'.

--- src/xsps.cxx
+++ src/xsps.cxx
@@ -2,10 +2,8 @@
 #include <Log.h>
 #include <parse_args.h>
 
-using namespace xsps;
-
 int main(int argc, char **argv) {
-	LogTypes LOG;
-	Log(LOG.WARN, "This program is currently useless.", "FIXME");
-	return parse_args(LOG, argc, argv);
+	xsps::LogTypes LOG;
+	xsps::Log(LOG.WARN, "This program is currently useless.", "FIXME");
+	return xsps::parse_args(LOG, argc, argv);
 }
