0ac28ab7dba2b191bb0fc8914295cc3b334ba1a3
davehome <davehome@redthumb.info.tm>
2012-02-26 22:35:57 -0700

    xsps.cc: remove call to deleted 'parse_args'

--- src/xsps.cc
+++ src/xsps.cc
@@ -1,20 +1,10 @@
 #include <common.h>
 #include <Log.h>
-#include <parse_args.h>
 #include <classes/Args.h>
 
 xsps::LogTypes LOG;
 
 int main(int argc, char **argv) {
-	xsps::Log(LOG.WARN, "This program is currently useless.", "FIXME");
-
-	// This is a test
-	xsps::Args a(argc, argv);
-	for(auto item : a.args) {
-		xsps::Log(LOG.DEBUG, item, "Args");
-	}
-
-	// parse_args will be deprecated soon.
-	return xsps::parse_args(LOG, argc, argv);
-	//return 0;
+	xsps::Args a(argc, argv, LOG);
+	return 0;
 }
