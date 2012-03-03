f3362129ed0be473a36f718116b5b0d405a22657
davehome <davehome@redthumb.info.tm>
2012-03-03 01:13:51 -0700

    xstrcpy: check non-zero size instead.

--- src/xsps_strcpy.c
+++ src/xsps_strcpy.c
@@ -8,7 +8,7 @@ char** xstrcpy(char** s1, char* s2) {
 	if((*s1 != NULL) && (strlen(*s1) != 0)) free(*s1);
 	if((*s1 = malloc(size)) != NULL) {
 		strncpy(*s1, s2, size);
-		if((*s1)[size-1] != '\0') (*s1)[size-1] = '\0';
+		if(size > 0) (*s1)[size-1] = '\0';
 		return s1;
 	}
 	return NULL;
