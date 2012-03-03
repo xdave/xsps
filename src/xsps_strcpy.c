a4459f25524243f62b33f2b33d3cd3f0e32d7028
davehome <davehome@redthumb.info.tm>
2012-03-03 01:29:17 -0700

    xstrcpy: conditionally make room for NULL byte.

--- src/xsps_strcpy.c
+++ src/xsps_strcpy.c
@@ -4,13 +4,15 @@
 #include "xsps.h"
 
 char** xstrcpy(char** s1, char* s2) {
-	size_t size = strlen(s2) + 1;
+	size_t size = strlen(s2);
+	if(s2[size-1] != '\0') size++;
 	if((*s1 != NULL) && (strlen(*s1) != 0)) free(*s1);
 	if((*s1 = malloc(size)) != NULL) {
 		strncpy(*s1, s2, size);
 		if(size > 0) (*s1)[size-1] = '\0';
 		return s1;
 	}
-	return NULL;
+	fprintf(stderr, "WARNING: \"%s\" not copied!\n", s2);
+	return s1;
 }
 
