5570c8057974f542ad9b6165ab8c8f6d9bfe15c4
davehome <davehome@redthumb.info.tm>
2012-03-04 13:14:41 -0700

    [all]: remove Juan's name from files he didn't 'touch'.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 /* Copyright (c) 2012 davehome <davehome@redthumb.info.tm>.
- * Copyright (c) 2012 Juan Romero Pardines.
  * All rights reserved.
  * Distributed under a modified BSD-style license.
  * See the COPYING file in the toplevel directory for license details. */
@@ -8,7 +7,6 @@
 #include <string.h>
 
 #include "xsps.h"
-#include "template.h"
 
 int
 main(int argc, char** argv)
