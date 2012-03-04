b7813261d085df965ac934a2b1e2aa9c157caee2
davehome <davehome@redthumb.info.tm>
2012-03-04 15:11:47 -0700

    main.c: print more debug msgs

--- src/main.c
+++ src/main.c
@@ -13,9 +13,19 @@ main(int argc, char **argv)
 {
 	xhp_t *xhp = xhp_new(argc, argv);
 
-	/* testing config args */
+	/* testing config */
 	log_debug(xhp, "%s", "Configuration:");
-	log_debug(xhp, "masterdir = '%s'", xhp->config->masterdir);
+	log_debug(xhp, "file         = '%s'", xhp->arg->config);
+	log_debug(xhp, "distdir      = '%s'", xhp->config->distdir);
+	log_debug(xhp, "repourl      = '%s'", xhp->config->repourl);
+	log_debug(xhp, "masterdir    = '%s'", xhp->config->masterdir);
+	log_debug(xhp, "hostdir      = '%s'", xhp->config->hostdir);
+	log_debug(xhp, "cflags       = '%s'", xhp->config->cflags);
+	log_debug(xhp, "cxxflags     = '%s'", xhp->config->cxxflags);
+	log_debug(xhp, "ldflags      = '%s'", xhp->config->ldflags);
+	log_debug(xhp, "compress_cmd = '%s'", xhp->config->compress_cmd);
+	log_debug(xhp, "ccache       = '%d'", xhp->config->ccache);
+	log_debug(xhp, "makejobs     = '%d'", xhp->config->makejobs);
 
 	process_template(xhp);
 
