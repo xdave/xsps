77e88a0dfdf238d60ccb08fcc1cf399e68aff715
Juan RP <xtraeme@gmail.com>
2012-03-04 08:24:48 +0100

    xsps.h: std function prototypes.

--- include/xsps.h
+++ include/xsps.h
@@ -74,7 +74,7 @@ typedef struct xsps_handle_t {
 	xsps_config_t* config;
 } xsps_handle_t;
 
-xsps_handle_t* xsps_handle_new();
+xsps_handle_t* xsps_handle_new(void);
 void xsps_handle_free(xsps_handle_t*);
 
 /* logging */
@@ -93,7 +93,7 @@ int xsps_arg_print_usage(xsps_handle_t*, const char*);
 void xsps_config_init(xsps_handle_t*);
 
 /* string manager*/
-void	xsps_strmgr_init();
+void	xsps_strmgr_init(void);
 void	xsps_strmgr_free(xsps_strmgr_t*);
 char*	xsps_strmgr_add(xsps_strmgr_t*, const char*);
 void	xsps_strmgr_del(xsps_strmgr_t*);
