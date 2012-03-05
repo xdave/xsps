df1b5101b0679e4c81570a5807f9da726e3f3979
davehome <davehome@redthumb.info.tm>
2012-03-05 00:37:51 -0700

    str.c: new functions: str_add_nocopy() and str_replace()

--- include/xsps.h
+++ include/xsps.h
@@ -112,9 +112,11 @@ void config_init(xhp_t *);
 void	str_init(xhp_t *);
 void	str_free(xhp_t *);
 char	*str_add(xhp_t *, const char *);
+char	*str_add_nocopy(xhp_t *, const char *);
 bool	xstreq(const char *, const char *);
 char	*xstrcpy(xhp_t *, const char *);
 char	*xstrfcpy(xhp_t *, const char *, ...);
+char	*str_replace(xhp_t *, const char *, const char *, const char *);
 
 /* template processing */
 int process_template(xhp_t *);
--- src/str.c
+++ src/str.c
@@ -31,6 +31,16 @@ str_add(xhp_t *xhp, const char *item)
 	return s->items[s->size-1];
 }
 
+char *
+str_add_nocopy(xhp_t *xhp, const char *item)
+{
+	str_t *s = xhp->str;
+	s->size++;
+	s->items = xrealloc(xhp, s->items, s->size * sizeof(char *));
+	s->items[s->size-1] = (char *)item;
+	return s->items[s->size-1];
+}
+
 /* removes a string -- never call this directly; use xsps_strmgr_free(). */
 static void
 str_del(xhp_t *xhp)
@@ -94,4 +104,49 @@ xstrfcpy(xhp_t *xhp, const char *fmt, ...)
 	result = str_add(xhp, p);
 	free(p);
 	return result;
+}
+
+/* Replaces all occurrences of pat with repl in orig. */
+char *
+str_replace(xhp_t *xhp, const char *orig, const char *pat, const char *repl)
+{
+	size_t orilen = strlen(orig);
+	size_t patlen = strlen(pat);
+	size_t replen = strlen(repl);
+	size_t patcnt = 0;
+	size_t skplen;
+	size_t retlen;
+
+	const char *oriptr = orig;
+	const char *patloc;
+	char *returned;
+	char *retptr;
+
+	/* find how many times the pattern occurs in the original string */
+	for (; (patloc = strstr(oriptr, pat)); oriptr = patloc + patlen)
+		patcnt++;
+
+	/* allocate memory for the new string */
+	retlen = orilen + patcnt * (replen - patlen);
+	returned = xmalloc(xhp, sizeof(char) * (retlen + 1));
+
+	if (returned != NULL) {
+		/* copy the original string,
+		 * replacing all the instances of the pattern */
+		retptr = returned;
+		oriptr = orig;
+		for (; (patloc = strstr(oriptr, pat))
+		     ; oriptr = patloc + patlen) {
+			skplen = (size_t)(patloc - oriptr);
+			/* copy the section until the occurence of pattern*/
+			strncpy(retptr, oriptr, skplen);
+			retptr += skplen;
+			/* copy the replacement */
+			strncpy(retptr, repl, replen);
+			retptr += replen;
+		}
+		/* copy the rest of the string. */
+		strcpy(retptr, oriptr);
+	}
+	return str_add_nocopy(xhp, returned);
 }
