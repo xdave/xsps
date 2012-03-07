060be13d04fd310500782ee695cb2ab2c01f90c1
davehome <davehome@redthumb.info.tm>
2012-03-07 15:12:25 -0700

    handle.c: tweak some (de)initialization.

--- src/handle.c
+++ src/handle.c
@@ -12,41 +12,47 @@ void
 xhp_init(int argc, char **argv)
 {
 	struct utsname u;
-	xhp = malloc(sizeof(xhp_t));
 	if (xhp == NULL) {
-		fprintf(stderr, "%s:%d: Out of memory!", __FILE__, __LINE__);
-		exit(EXIT_FAILURE);
+		xhp = malloc(sizeof(xhp_t));
+		if (xhp == NULL && sizeof(xhp_t) > 0) {
+			fprintf(stderr, "%s:%d: Out of memory!",
+					__FILE__, __LINE__);
+			exit(EXIT_FAILURE);
+		}
+		xhp->init = 1;
+		xhp->log_file = NULL;
+		uname(&u);
+		str_init();
+		arg_init(argc, argv);
+		log_set_file("test.log");
+		xhp->config = NULL;
+		config_init();
+		setenv("XSPS_MACHINE", u.machine, 1);
+		setenv("XSPS_VENDOR", "void", 1);
+		setenv("XSPS_KERNEL", "linux", 1);
+		setenv("XSPS_OS", "gnu", 1);
+		setenv("XSPS_BUILD_TRIPLET", breplace(
+		    "${XSPS_MACHINE}-${XSPS_VENDOR}-"
+		    "${XSPS_KERNEL}-${XSPS_OS}"), 1);
+		log_debug("Welcome to XSPS on %s!",
+		    getenv("XSPS_BUILD_TRIPLET"));
 	}
-	str_init();
-	arg_init(argc, argv);
-
-	log_set_file("test.log");
-
-	config_init();
-
-	uname(&u);
-	setenv("XSPS_MACHINE", u.machine, 1);
-	setenv("XSPS_VENDOR", "void", 1);
-	setenv("XSPS_KERNEL", "linux", 1);
-	setenv("XSPS_OS", "gnu", 1);
-	setenv("XSPS_BUILD_TRIPLET", breplace(
-	    "${XSPS_MACHINE}-${XSPS_VENDOR}-${XSPS_KERNEL}-${XSPS_OS}"), 1);
-
-	log_debug("Welcome to XSPS on %s!", getenv("XSPS_BUILD_TRIPLET"));
 }
 
 void
 xhp_free()
 {
-	str_free();
+	if (xhp != NULL) {
+		xhp->init = 0;
+		if (xhp->str != NULL) str_free();
+		if (xhp->config != NULL && xhp->config->cfg != NULL)
+			cfg_free(xhp->config->cfg);
 
-	if (xhp->config != NULL && xhp->config->cfg != NULL)
-		cfg_free(xhp->config->cfg);
+		if (xhp->config != NULL) free(xhp->config);
+		if (xhp->arg != NULL) free(xhp->arg);
 
-	if (xhp->config != NULL) free(xhp->config);
-	if (xhp->arg != NULL) free(xhp->arg);
+		log_close();
 
-	log_close();
-
-	free(xhp);
+		free(xhp);
+	}
 }
